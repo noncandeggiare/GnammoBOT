3b655b6e346ca3095a99a4ad9d6580ce7280c53a
Filippo Buscaroli <filippobuscaroli@outlook.it>
2023-06-13 13:46:50 +0200

    Versione iniziale dell'estrattore

--- EstrattoreGnammoBOT.py
+++ EstrattoreGnammoBOT.py
@@ -1,35 +1,35 @@
-# GnammoBOT
+#GnammoBOT
 
 import requests
-import BeautifulSoup
+from bs4 import BeautifulSoup
+import datetime
 
-# URL del sito
-url = "https://www.mazzotti.org/bassaromagnacatering/"
+# Get today's date
+today = datetime.date.today()
 
-# Parametri per la selezione del menu
-comune_value = "Massa Lombarda"
-grado_value = "Infanzia"
-data_value = "2023-06-13"  # Sostituisci con la data desiderata nel formato "AAAA-MM-GG"
+# URL for the catering menu
+url = f"https://www.mazzotti.org/bassaromagnacatering/calendar.php?comune=7&grado=2&giorno={today}"
 
-# Costruisci il payload per la richiesta POST
-payload = {
-    "comune": comune_value,
-    "grado": grado_value,
-    "giorno": data_value
-}
+# Send a GET request to the URL with certificate verification disabled
+response = requests.get(url, verify=False)
 
-# Effettua la richiesta POST per ottenere il menu
-response = requests.post(url, data=payload)
+# Create a BeautifulSoup object to parse the HTML content
+soup = BeautifulSoup(response.content, "html.parser")
 
-# Analizza il contenuto HTML della risposta
-soup = BeautifulSoup(response.text, "html.parser")
+# Find the div element with the class "fc-day-number" containing today's day number
+day_element = soup.find("div", class_="fc-day-number", text=str(today.day))
 
-# Trova il menu del giorno
-menu_elements = soup.select("div.container-fluid div.row div.col-sm-6.menu div.row div.col-xs-8.data")
+# Find the parent div element with class "fc-div-content"
+div_content_element = day_element.find_parent("div", class_="fc-div-content")
 
-# Estrai il testo del menu
-menu = [menu_element.text.strip() for menu_element in menu_elements]
+# Find the nested div element with class "menu-calendar"
+menu_element = div_content_element.find("div", class_="menu-calendar")
 
-# Stampa il menu
-for item in menu:
-    print(item)
+# Check if menu is available for today
+if menu_element is not None:
+    # Extract the menu text
+    menu_text = menu_element.get_text(separator="\n").strip()
+    print("Menu for today:")
+    print(menu_text)
+else:
+    print("No menu available for today.")
